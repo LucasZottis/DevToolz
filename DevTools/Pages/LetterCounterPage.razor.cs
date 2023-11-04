26f2fa333d8261e17af087629e5b1c3d3fbea746
Lucas Zottis <zottis.lucas@hotmail.com>
2023-11-04 12:23:49 -0300

    Implementado página para contar letras

---
+++ DevTools/Pages/LetterCounterPage.razor.cs
@@ -0,0 +1,15 @@
+﻿using BibliotecaPublica.Core.Extensions;
+
+namespace DevTools.Pages;
+
+public partial class LetterCounterPage : ComponentBase
+{
+    private string _text;
+    private int _letterAmount;
+
+    private async Task OnClickButton()
+    {
+        if ( _text.TemConteudo() )
+            _letterAmount = _text.Length;
+    }
+}
