48733b50168160aeee098f34b6362f50e337a02e
Lucas Zottis <zottis.lucas@hotmail.com>
2024-10-04 12:54:37 -0300

    Commit

---
+++ dev-tools/src/pages/conversors/upper-to-lower-letters-conversor-page/upper-to-lower-letters-conversor-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'upper-to-lower-letters-conversor-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './upper-to-lower-letters-conversor-page.component.html',
+  styleUrl: './upper-to-lower-letters-conversor-page.component.scss'
+})
+export class UpperToLowerLettersConversorPageComponent {
+
+}
