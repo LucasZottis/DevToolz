9c1b39a58b8f671a3940e96b3fe0a5f6259f5869
Lucas Zottis <63120165+LucasZottis@users.noreply.github.com>
2024-10-04 17:53:23 +0000

    Commit

--- dev-tools/src/pages/conversors/upper-to-lower-letters-conversor-page/upper-to-lower-letters-conversor-page.component.ts
+++ dev-tools/src/pages/conversors/upper-to-lower-letters-conversor-page/upper-to-lower-letters-conversor-page.component.ts
@@ -10,6 +10,40 @@ import { PageBase } from '../../pageBase';
 })
 
 export class UpperToLowerLettersConversorPageComponent extends PageBase implements OnInit {
+  text: string = "";
+
+  private _convertToUpper(text: string): string {
+    return text.toUpperCase();
+  }
+
+  private _convertToLower(text: string): string {
+    return text.toLowerCase();
+  }
+
+  private _convertToAlternated(text: string): string {
+    let convertedText: string = "";
+    for(let i: number = 0; i < text.length; i++) {
+      if (i % 2 == 0)
+        convertedText += text[i].toLowerCase();
+      else
+        convertedText += text[i].toUpperCase();
+    }
+
+    return convertedText;
+  }
+
+  private _convertInvertedText(text: string): string {
+    let convertedText: string = "";
+    for(let i: number = 0; i < text.length; i++) {
+      if (text[i] === text[i].toUpperCase())
+        convertedText += text[i].toLowerCase();
+      else
+        convertedText += text[i].toUpperCase();
+    }
+
+    return convertedText;
+  }
+  
   ngOnInit(): void {
     this.addDescription("Transformar textos para maiúsculas ou para minúsculas? Basta usar nossa ferramenta.")
   }
