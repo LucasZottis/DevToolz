b88405e490e0daa0319d0e4b0fc949b3e197d0be
Lucas Zottis <zottis.lucas@hotmail.com>
2025-03-19 21:15:47 -0300

    AJustes nas meta tags

--- dev-tools/src/pages/home-page/home-page.component.ts
+++ dev-tools/src/pages/home-page/home-page.component.ts
@@ -14,6 +14,10 @@ import { PageBase } from '../pageBase';
 })
 export class HomePageComponent extends PageBase implements OnInit {
   ngOnInit(): void {
-    this.addDescription('As melhores ferramentas para desenvolvedores estão aqui!');
+    // this.addDescription('As melhores ferramentas para desenvolvedores estão aqui!');
+    this.title = 'DevToolz - Ferramentas para Desenvolvedores';
+    this.description = 'DevToolz oferece ferramentas online úteis para desenvolvedores, como conversores, geradores e outras soluções para otimizar tarefas no desenvolvimento de software.';
+    this.robots = 'index, follow';
+    this.keywords = 'ferramentas para desenvolvedores, conversores online, conversor de tempo, gerador de CPF, gerador de CNPJ, utilitários para desenvolvimento de software, ferramentas úteis para desenvolvedores';
   }
 }
