abcfd7e0d10ff772c85768d1803cd6381ff79c4d
Lucas Zottis <zottis.lucas@hotmail.com>
2025-03-19 23:14:27 -0300

    Adicionado meta tag

--- dev-tools/src/pages/remover-acento-texto-page/remover-acento-texto-page.component.ts
+++ dev-tools/src/pages/remover-acento-texto-page/remover-acento-texto-page.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PageBase } from '../pageBase';
 
 @Component({
   selector: 'remover-acento-texto-page',
@@ -11,11 +12,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './remover-acento-texto-page.component.scss'
 })
 
-export class RemoverAcentoTextoPageComponent {
+export class RemoverAcentoTextoPageComponent extends PageBase implements OnInit {
   text: string = "";
   treatedText: string = "";
 
   onClick() {
     this.treatedText = this.text.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+  }
+
+  ngOnInit(): void {
+    this.title = 'Remover Acentuação Online';
+    this.description = 'Remova acentos de textos rapidamente com nossa ferramenta online. Transforme palavras acentuadas em versões sem acento com um clique.';
+    this.robots = 'index, follow';
+    this.keywords = 'remover acentuação, remover acentos online, converter texto sem acento, retirar acentos, remover caracteres especiais';
   }
 }
